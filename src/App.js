bb99048661b0c345d432c54b78d1e2b8ccc6ac7e
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-03-12 14:37:05 +0330

    Add box shadow color

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [ver, setver] = useState(10)
   const [blur, setblur] = useState(10)
   const [color, setcolor] = useState('rgb(0 196 255)')
+  const [ShadowColor, setShadowColor] = useState('#000')
 
 
   return (
@@ -18,12 +19,15 @@ function App() {
         <input type="range" min="-200" onChange={e=> setver(e.target.value)} value={ver} max="200" />
         <label>Blur</label>
         <input type="range" min="-200" onChange={e=> setblur(e.target.value)} value={blur} max="200" />
-       <h6>color </h6>
-        <input type="color" onChange={e=> setcolor(e.target.value)} value={color}/>
+       <h6>Color </h6>
+        <input type="color" value={color} onChange={e=> setcolor(e.target.value)}/>
+        <p>Shadow color</p>
+        <input type="color" value={ShadowColor} onChange={e=> setShadowColor(e.target.value)}/>
         </p>
       </div>
       <div className="output">
-        <div className="box" style={{boxShadow: `${hor}px ${ver}px ${blur}px`, background: `${color}`}}></div>
+        <div className="box" style={{boxShadow: `${hor}px ${ver}px ${blur}px ${ShadowColor}`, background: `${color}`}}>  <p id="result">box-shadow:{hor}px {ver}px {blur}px {ShadowColor}</p></div>
+      
       </div>
     </div>
   );
