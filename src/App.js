754dc7e460cca7a4d6fefe10be1f7db30af4b5a5
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-03-12 14:18:52 +0330

    Add color option

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [hor, sethor] = useState(10)
   const [ver, setver] = useState(10)
   const [blur, setblur] = useState(10)
+  const [color, setcolor] = useState('rgb(0 196 255)')
 
 
   return (
@@ -17,10 +18,12 @@ function App() {
         <input type="range" min="-200" onChange={e=> setver(e.target.value)} value={ver} max="200" />
         <label>Blur</label>
         <input type="range" min="-200" onChange={e=> setblur(e.target.value)} value={blur} max="200" />
+       <h6>color </h6>
+        <input type="color" onChange={e=> setcolor(e.target.value)} value={color}/>
         </p>
       </div>
       <div className="output">
-        <div className="box"></div>
+        <div className="box" style={{boxShadow: `${hor}px ${ver}px ${blur}px`, background: `${color}`}}></div>
       </div>
     </div>
   );
