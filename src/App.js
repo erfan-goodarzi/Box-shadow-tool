3759f5823fd6546913314380fa6c65042f64b25b
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-03-13 17:25:44 +0330

    Change app.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
@@ -12,21 +12,21 @@ function App() {
   return (
     <div className="App">
       <div className="controls">
-        <p class="range-field">
+        <p className="range-field">
           <label>horizontal lenght</label>
-        <input type="range" min="-200" onChange={e=> sethor(e.target.value)} value={hor} max="200" />
-        <label>Verticle lenght</label>
-        <input type="range" min="-200" onChange={e=> setver(e.target.value)} value={ver} max="200" />
-        <label>Blur</label>
-        <input type="range" min="-200" onChange={e=> setblur(e.target.value)} value={blur} max="200" />
-       <h6>Color </h6>
-        <input type="color" value={color} onChange={e=> setcolor(e.target.value)}/>
-        <p>Shadow color</p>
-        <input type="color" value={ShadowColor} onChange={e=> setShadowColor(e.target.value)}/>
+          <input type="range" min="-200" onChange={e => sethor(e.target.value)} value={hor} max="200" />
+          <label>Verticle lenght</label>
+          <input type="range" min="-200" onChange={e => setver(e.target.value)} value={ver} max="200" />
+          <label>Blur</label>
+          <input type="range" min="-200" onChange={e => setblur(e.target.value)} value={blur} max="200" />
+          <h6>Color </h6>
+          <input type="color" value={color} onChange={e => setcolor(e.target.value)} />
+          <p>Shadow color</p>
+          <input type="color" value={ShadowColor} onChange={e => setShadowColor(e.target.value)} />
         </p>
       </div>
       <div className="output">
-        <div className="box" style={{boxShadow: `${hor}px ${ver}px ${blur}px ${ShadowColor}`, background: `${color}`}}>  <p id="result">box-shadow:{hor}px {ver}px {blur}px {ShadowColor}</p></div>
+        <div className="box" style={{ boxShadow: `${hor}px ${ver}px ${blur}px ${ShadowColor}`, background: `${color}` }}>  <p id="result">box-shadow:{hor}px {ver}px {blur}px {ShadowColor}</p></div>
       
       </div>
     </div>
