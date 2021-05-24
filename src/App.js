143ad7bb7431837af049b5cb6b36a23fec6da509
Erfan Goodarzi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-05-25 01:03:35 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <div className="controls">
-        <p className="range-field">
+        <div className="range-field">
           <label>horizontal lenght</label>
           <input type="range" min="-200" onChange={e => sethor(e.target.value)} value={hor} max="200" />
           <label>Verticle lenght</label>
@@ -26,7 +26,7 @@ function App() {
           <input type="color" value={color} onChange={e => setcolor(e.target.value)} />
           <p>Shadow color</p>
           <input type="color" value={ShadowColor} onChange={e => setShadowColor(e.target.value)} />
-        </p>
+        </div>
       </div>
       <div className="output">
         <div className="box" style={{ boxShadow: `${hor}px ${ver}px ${blur}px ${ShadowColor}`, background: `${color}` , borderRadius:`${BorderRadius}px` }}>  <p id="result">box-shadow:{hor}px {ver}px {blur}px {ShadowColor}  </p></div>
